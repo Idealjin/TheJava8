3dc960cca504bf0e307c7b258bcf3682de5b847a
idealjin <idealjin316@gmail.com>
2022-08-30 18:22:44 +0900

    Date And Time22

--- src/main/java/java8to11/DateAndTime.java
+++ src/main/java/java8to11/DateAndTime.java
@@ -14,7 +14,7 @@ public class DateAndTime {
 
         //-> 불편햇다 불편한점은 https://codeblog.jonskeet.uk/2017/04/23/all-about-java-util-date/ 여기
         Calendar sangjinBirthDat = new GregorianCalendar(1990, Calendar.OCTOBER, 22);
-        // -> 10월은 숫자로 9로 표현 (0부터 1월이기떄문에)
+        // -> 10월은 숫자로 9로 표현 (0부터 1월이기떄문에) -> 불편한점. 그래서 OCTOBER 이런식으로 상수로 써준다.
 
         Long time = date.getTime();
         //-> 기계용 시간
